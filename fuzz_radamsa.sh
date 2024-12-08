ce0432a5cb3fe8f9856791e5b04c2f9ff3e4fe9e
cristianbass01 <cristianbassotto21@gmail.com>
2024-12-08 02:24:54 +0100

    asan executable

--- fuzz_radamsa.sh
+++ fuzz_radamsa.sh
@@ -7,6 +7,15 @@ fi
 
 EXECUTABLE="$1"
 
+if [[ "$EXECUTABLE" == *asan* ]]; then
+    SANITIZER="asan"
+elif [[ "$EXECUTABLE" == *msan* ]]; then
+    SANITIZER="msan"
+else
+    SANITIZER=""
+fi
+
+
 # Directory containing the initial seed files
 SEED_DIR="./images"  # Directory with initial seed files (e.g., PNG files)
 echo "Using executable: $EXECUTABLE"
@@ -14,12 +23,15 @@ echo "Using executable: $EXECUTABLE"
 SAVE_ALL_LOGS=0  # Set to 1 to save all logs, including successful runs
 
 # Directories to save mutated files and crash logs
-RADAMSA_DIR="./output/radamsa"
+RADAMSA_DIR="./output/radamsa_$SANITIZER" 
 MUTATED_DIR="$RADAMSA_DIR/mutated"
 TMP_LOG_FILE="$RADAMSA_DIR/tmp_log.txt"
 LOG_FILE="$RADAMSA_DIR/logs.txt"
 ERROR_LOG_DIR="$RADAMSA_DIR/error_logs"
 SEGM_FAULT_LOG_DIR="$RADAMSA_DIR/seg_fault_logs"
+
+ERROR_CRASH_REPORT="$ERROR_LOG_DIR/_crash_report.txt"
+SEGM_FAULT_CRASH_REPORT="$SEGM_FAULT_LOG_DIR/_crash_report.txt"
 
 rm -rf $RADAMSA_DIR
 
@@ -43,11 +55,12 @@ while true; do
         SEED_NAME=$(basename "$SEED_FILE")  # Get the filename without the path
         SEED_NAME="${SEED_NAME%.*}"  # Remove the file extension
 
+        # Msan crashes on some seed files
         # if the seed name starts with ct and ends with n0g04, skip it (but not if the char in the middle is 0)
-        if [[ "$SEED_NAME" =~ ^ct.*n0g04$ && ! "$SEED_NAME" =~ ^ct0.*n0g04$ ]]; then
+        #if [[ "$SEED_NAME" =~ ^ct.*n0g04$ && ! "$SEED_NAME" =~ ^ct0.*n0g04$ ]]; then
             #echo "Skipping seed file: $SEED_NAME"
-            continue
-        fi
+        #    continue
+        #fi
 
         # Generate mutated versions from seed file
         SEED_NUMBER=$((SEED_NUMBER + 1))
@@ -56,12 +69,19 @@ while true; do
         MUTATED_FILES="$MUTATED_DIR/${SEED_NAME}_%n.png"
 
         radamsa -o $MUTATED_FILES -s $SEED_NUMBER -n $MUTATIONS_PER_SEED_FILE $SEED_FILE
+        EXIT_STATUS=$?
+
+        if [ $EXIT_STATUS -ne 0 ]; then
+            echo "Radamsa failed with exit status $EXIT_STATUS: "
+            echo "Command radamsa -o $MUTATED_FILES -s $SEED_NUMBER -n $MUTATIONS_PER_SEED_FILE $SEED_FILE"
+            continue
+        fi
 
         for MUTATED_FILE in $MUTATED_DIR/*; do
             # Run the mutated file through the program that uses libspng
             # Capture the exit status of the program
             #strace -e trace=write $EXECUTABLE $MUTATED_FILE > $TMP_LOG_FILE 2>&1
-            $EXECUTABLE $MUTATED_FILE > $TMP_LOG_FILE 2>&1
+            timeout 4s $EXECUTABLE $MUTATED_FILE > $TMP_LOG_FILE 2>&1
             EXIT_STATUS=$?
 
             if [ $SAVE_ALL_LOGS -eq 1 ]; then
@@ -83,13 +103,13 @@ while true; do
                 COUNTER_SEG_FAULTS=$((COUNTER_SEG_FAULTS + 1))
 
                 # Log the segmentation fault and the mutated file
-                echo "Segmentation fault detected on seed number $SEED_NUMBER for seed file: $SEED_NAME!" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                echo "Counter at: $COUNTER" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                echo "Mutated File: $MUTATED_FILE" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                echo "Exit Status: $EXIT_STATUS" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                echo "Segmentation Output:" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                cat $TMP_LOG_FILE >> $SEGM_FAULT_LOG_DIR/crash_report.txt
-                echo "----------------------------------------------------" >> $SEGM_FAULT_LOG_DIR/crash_report.txt
+                echo "Segmentation fault detected on seed number $SEED_NUMBER for seed file: $SEED_NAME!" >> $SEGM_FAULT_CRASH_REPORT
+                echo "Counter at: $COUNTER" >> $SEGM_FAULT_CRASH_REPORT
+                echo "Mutated File: $MUTATED_FILE" >> $SEGM_FAULT_CRASH_REPORT
+                echo "Exit Status: $EXIT_STATUS" >> $SEGM_FAULT_CRASH_REPORT
+                echo "Error Output:" >> $SEGM_FAULT_CRASH_REPORT
+                cat $TMP_LOG_FILE >> $SEGM_FAULT_CRASH_REPORT
+                echo "----------------------------------------------------" >> $SEGM_FAULT_CRASH_REPORT
 
                 # Save the file with a unique name based on the mutation count
                 cp $MUTATED_FILE $SEGM_FAULT_LOG_DIR/${SEED_NAME}_$COUNTER.png
@@ -98,14 +118,18 @@ while true; do
                 # Increment the error count
                 COUNTER_ERRORS=$((COUNTER_ERRORS + 1))
 
+                if [ $EXIT_STATUS -eq 124 ]; then
+                    echo "Timeout reached for file: $MUTATED_FILE" >> $LOG_FILE
+                fi
+
                 # Log the crash or other errors and the mutated file
-                echo "Error detected on seed number $SEED_NUMBER for seed file: $SEED_NAME!" >> $ERROR_LOG_DIR/crash_report.txt
-                echo "Counter at: $COUNTER" >> $ERROR_LOG_DIR/crash_report.txt
-                echo "Mutated File: $MUTATED_FILE" >> $ERROR_LOG_DIR/crash_report.txt
-                echo "Exit Status: $EXIT_STATUS" >> $ERROR_LOG_DIR/crash_report.txt
-                echo "Error Output:" >> $ERROR_LOG_DIR/crash_report.txt
-                cat $TMP_LOG_FILE >> $ERROR_LOG_DIR/crash_report.txt
-                echo "----------------------------------------------------" >> $ERROR_LOG_DIR/crash_report.txt
+                echo "Error detected on seed number $SEED_NUMBER for seed file: $SEED_NAME!" >> $ERROR_CRASH_REPORT
+                echo "Counter at: $COUNTER" >> $ERROR_CRASH_REPORT
+                echo "Mutated File: $MUTATED_FILE" >> $ERROR_CRASH_REPORT
+                echo "Exit Status: $EXIT_STATUS" >> $ERROR_CRASH_REPORT
+                echo "Error Output:" >> $ERROR_CRASH_REPORT
+                cat $TMP_LOG_FILE >> $ERROR_CRASH_REPORT
+                echo "----------------------------------------------------" >> $ERROR_CRASH_REPORT
 
                 # Save the file with a unique name based on the mutation count
                 cp $MUTATED_FILE $ERROR_LOG_DIR/${SEED_NAME}_$COUNTER.png
